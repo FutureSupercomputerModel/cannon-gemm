a5fbedfc6ce65753ff28c90a1bfaad8a5e0efb8b
SiyuanMa0316 <masiyuan0@gmail.com>
2024-09-20 11:29:06 -0500

    add experiments

--- arch_lib.py
+++ arch_lib.py
@@ -17,7 +17,17 @@ hier_arch_leaf_imec = Leaf(pe_arr_dim=200.0,
                       interconnect_nJ_per_bit=1e-8, 
                       buffer_nJ_per_bit=0.021e-6, 
                       bytes_per_element=2)
+hier_arch_leaf_imec_64bit = Leaf(pe_arr_dim=200.0, 
+                      buffer_size='20.0MB', 
+                      buffer_bw = f'{64*30.0/8}GBps',
+                      pe_freq=30.0, 
+                      nJ_per_mac=14e-6, 
+                      interconnect_nJ_per_bit=1e-8, 
+                      buffer_nJ_per_bit=0.021e-6, 
+                      bytes_per_element=2)
 
 hier_arch_blade = Arch(mesh_dim=9.0, mesh_bw=f'{200*8*30/8}GBps', buffer_size="8GB", buffer_bw='30.0TBps', mesh_nJ_per_bit=5e-7, buffer_nJ_per_bit=0.397e-3, child_arch=hier_arch_leaf_imec)
 hier_arch_blade_1leaf = Arch(mesh_dim=1.0, mesh_bw=f'{200*8*30/8}GBps', buffer_size="8GB", buffer_bw='30.0TBps', mesh_nJ_per_bit=5e-7, buffer_nJ_per_bit=0.397e-3, child_arch=hier_arch_leaf_imec)
+hier_arch_blade_1leaf_64bit = Arch(mesh_dim=1.0, mesh_bw=f'{200*8*30/8}GBps', buffer_size="8GB", buffer_bw='30.0TBps', mesh_nJ_per_bit=5e-7, buffer_nJ_per_bit=0.397e-3, child_arch=hier_arch_leaf_imec_64bit)
+hier_arch_blade_64bit = Arch(mesh_dim=9.0, mesh_bw=f'{64*30/8}GBps', buffer_size="8GB", buffer_bw='30.0TBps', mesh_nJ_per_bit=5e-7, buffer_nJ_per_bit=0.397e-3, child_arch=hier_arch_leaf_imec)
 hier_arch_node = Arch(mesh_dim=10.0, mesh_bw='1PBps', buffer_size="800GB", buffer_bw='3.0PBps', mesh_nJ_per_bit=5e-6, buffer_nJ_per_bit=0.397e-3, child_arch=hier_arch_blade)
